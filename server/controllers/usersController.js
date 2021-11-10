a14c4ccce609009416529ef0feec47a8208cc1c4
AlvaroCabero <alvarocabero@gmail.com>
2021-11-10 22:06:25 +0100

    Added LoginUser Controller

---
+++ server/controllers/usersController.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const User = require("../../database/models/user");
+
+const loginUser = async (req, res, next) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) {
+    const error = new Error("Wrong credentials");
+    error.code = 401;
+    next(error);
+  } else {
+    const rightPassword = await bcrypt.compare(password, user.password);
+    if (!rightPassword) {
+      const error = new Error("Wrong credentials");
+      error.code = 401;
+      next(error);
+    } else {
+      const token = jwt.sign(
+        {
+          id: user.id,
+          name: user.name,
+        },
+        process.env.SECRET,
+        {
+          expiresIn: 24 * 60 * 60,
+        }
+      );
+      req.json({ token });
+    }
+  }
+};
+
+module.exports = { loginUser };
