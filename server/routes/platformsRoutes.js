6eb18c0fafa787472d28e539e9d1fc930777e5ec
Franny Medico <m.francisca.medico@gmail.com>
2021-11-10 21:39:17 +0100

    added routes platform

--- server/routes/platformsRoutes.js
+++ server/routes/platformsRoutes.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const { validate } = require("express-validation");
+const {
+  updatePlatformSchema,
+  platformSchema,
+  deletePlatformSchema,
+} = require("../schemas/platformSchemas");
 
 const router = express.Router();
 
 router.get("/", getPlatforms);
 
-router.post("/", createPlatform);
+router.post("/", validate(platformSchema), createPlatform);
 
-router.put("/:idPlatform", updatePlatform);
+router.put("/:idPlatform", validate(updatePlatformSchema), updatePlatform);
 
-router.delete("/:idPlatform", deletePlatform);
+router.delete("/:idPlatform", validate(deletePlatformSchema), deletePlatform);
