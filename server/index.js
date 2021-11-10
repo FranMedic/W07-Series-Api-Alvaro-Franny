2ae62257ff55b5039802760e234e7f43c476d15f
Franny Medico <m.francisca.medico@gmail.com>
2021-11-10 18:50:48 +0100

    added initizialice server function with promise and imports necesary

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const debug = require("debug")("series:server");
+const morgan = require("morgan");
+const chalk = require("chalk");
+
+const app = express();
+
+const initializeServer = (port) =>
+  new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
+      debug(chalk.magentaBright(`Listen to port: ${port}`));
+    });
+
+    server.on("error", (error) => {
+      debug(chalk.red("Error appeared"));
+      if (error.code === "EADDRINUSE") {
+        debug(chalk.red(`The port ${port} is already in use (╯°□°）╯︵ ┻━┻`));
+        reject(error);
+      }
+    });
+    resolve(server);
+  });
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  debug(chalk.green("REQUEST ARRIVED ʕง•ᴥ•ʔง"));
+  next();
+});
