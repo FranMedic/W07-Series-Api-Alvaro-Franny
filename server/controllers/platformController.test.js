8a9168cde67bd1d5ad4562079f67402dfa17328c
Franny Medico <m.francisca.medico@gmail.com>
2021-11-11 19:33:34 +0100

    added platformController Test and added user to DB

--- server/controllers/platformController.test.js
+++ server/controllers/platformController.test.js
@@ -1,15 +1,71 @@
 const Platform = require("../../database/models/platform");
-const { getFakePlatforms } = require("../../factories/platformsFactory");
+const {
+  getFakePlatforms,
+  getFakePlatform,
+} = require("../../factories/platformsFactory");
+const { getPlatforms, createPlatform } = require("./platformControllers");
 
 jest.mock("../../database/models/platform");
+
+const mockResponse = () => {
+  const res = {};
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+  return res;
+};
 
 describe("Given a getPlatforms function", () => {
   describe("When it receives an object res", () => {
     test("then it should invoke the method res.json", async () => {
       const platforms = getFakePlatforms();
       Platform.find = jest.fn().mockResolvedValue(platforms);
+      const res = {
+        json: jest.fn(),
+      };
 
-      console.log(platforms);
+      await getPlatforms(null, res);
+
+      expect(Platform.find).toHaveBeenCalled();
+      expect(res.json).toHaveBeenCalledWith(platforms);
+    });
+  });
+});
+
+describe("Given a createPlatform function", () => {
+  describe("When it receives a req object with a body, a res object and a next function", () => {
+    test("Then it should invoke the method json of res with the platform sended and have an status 201", async () => {
+      const onePlatform = getFakePlatform();
+      const req = {
+        body: onePlatform,
+      };
+      Platform.create = jest.fn().mockResolvedValue(onePlatform);
+
+      const res = mockResponse();
+      const next = () => {};
+
+      await createPlatform(req, res, next);
+
+      expect(Platform.create).toHaveBeenCalled();
+      expect(res.json).toHaveBeenCalledWith(onePlatform);
+      expect(res.status).toHaveBeenCalledWith(201);
+    });
+  });
+
+  describe("And Platform.create() rejects", () => {
+    test("Then it should invoke the next function with the error ", async () => {
+      const error = {
+        message: "Error Happened",
+        code: 400,
+      };
+      Platform.create = jest.fn().mockRejectedValue(error);
+      const req = {};
+      const res = {};
+      const next = jest.fn();
+
+      await createPlatform(req, res, next);
+
+      expect(next).toHaveBeenCalled();
+      expect(next).toHaveBeenCalledWith(error);
     });
   });
 });
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const { validate } = require("express-validation");
+// const bcrypt = require("bcrypt");
+// const { default: ObjectID } = require("bson-objectid");
+// const User = require("../../database/models/user");
 const { loginUser } = require("../controllers/usersController");
 
 const { loginRequestSchema } = require("../schemas/userSchemas");
 
 const router = express.Router();
+/* router.get("/", async () => {
+  User.create({
+    name: "FrannyAlvaro",
+    username: "frannyalvaro",
+    password: await bcrypt.hash("frannyalvaro", 10),
+    isAdmin: true,
+    series: [ObjectID()],
+  });
+}); */
 
 router.post("/login", validate(loginRequestSchema), loginUser);
 
